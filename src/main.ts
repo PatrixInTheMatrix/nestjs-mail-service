2c5c94144587f6236b1ca8e4b3022b8b78c8127a
Patrice Perrin <p.perrin@diwidi.net>
2025-06-30 08:25:55 +0200

    chore(cors): allow diwidi domains and localhost for dev

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,15 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://www.deine-domain.de',
+    origin: [
+      'https://diwidi.net',
+      'https://www.diwidi.net',
+      'https://dev.diwidi.net',
+      'http://localhost:4200', // optional: nur im dev
+    ],
+    methods: ['POST'],
   });
+
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(3000);
 }
